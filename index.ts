cc25090a989e23cf5a3e0695c987e8ed8d467e39
Your Name <you@example.com>
2024-07-28 15:02:24 +0530

    cover shopping module

--- index.ts
+++ index.ts
@@ -1,27 +1,17 @@
 import express from "express";
-import cors from "cors";
-import path from "path";
-import bodyParser from "body-parser";
-import mongoose from "mongoose";
-import { AdminRoute, VendorRoute } from "./routes";
-import { MONGO_URI } from "./config";
+import App from "./services/Express";
+import dbConnection from "./services/Database";
 
-const app = express();
+const StartServer = async () => {
+  const app = express();
 
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use("/images", express.static(path.join(__dirname, "images")));
-mongoose
-  .connect(MONGO_URI)
-  .then((result) => {
-    // console.log(result);
-    console.log("Connected");
-  })
-  .catch((err) => console.log(err));
+  await dbConnection();
 
-app.use("/vendor", VendorRoute);
-app.use("/admin", AdminRoute);
+  await App(app);
 
-app.listen(8000, () => {
-  console.log("APP start listening on Port 8000");
-});
+  app.listen(8000, () => {
+    console.log("Listening to port 8000");
+  });
+};
+
+StartServer();
